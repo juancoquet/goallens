6e738f641dae706b34cf90e73845724471a5f856
juancoquet <juancoquet@gmail.com>
2022-05-25 15:21:12 +0100

    write basic tests for team scraper

--- data_sourcing/tests/test_scrapers.py
+++ data_sourcing/tests/test_scrapers.py
@@ -1,33 +1,59 @@
-# from unittest import TestCase
-# from scrapers.teams_scraper import TeamScraper
+from unittest import TestCase
+from data_sourcing.scrapers.teams.teams_scraper import TeamScraper
 
 
-# class TestTeamScraper(TestCase):
+# need to get:
+# - team id
+# - team name
+# - team short name
 
-#     # go to pl page
-#     # (get list of all teams' ids from pl table for a given season)
-#     # go to each team's url
-#     # get full team name
-#     # add team name and id to db
+# keep scrapers separate from data handlers (modules/functions that send data to the database)
 
-#     # can 
+# for a given league on a given season:
+# - scrape all team ids from league table
+# - scrape all team short names from league table
+# - scrape all team names from league table
+    # - take list of team ids as argument
+    # - go to each team's page
+    # - scrape team name
 
-#     def test_get_all_team_ids(self):
-#         output = TeamScraper.get_all_team_ids('2019-2020', league='Premier League')
-#         expected = [
-#             '822bd0ba', 'b8fd03ef', '19538871', 'cff3d9bb', 'a2d435b3', # liv, mci, mutd, che, lei
-#             '361ca564', '8cec06e1', '18bb7c10', '1df6b87e', '943e8050', # tot, wol, ars, shef, bur
-#             '33c895d4', 'd3fd31cc', 'b2b47a98', '47c64c55', 'd07537b9', # sou, eve, newc, cry, bri
-#             '7c21e445', '8602292d', '4ba7cbea', '2abfe087', '1c781004', # wham, av, bou, wat, nor
-#         ]
-#         self.assertEqual(len(output), 20)
-#         self.assertEqual(output, expected)
 
-#     def test_get_team_names(self):
-#         output = TeamScraper.get_team_names(['822bd0ba', '19538871', 'd07537b9']) # liv, mutd, bri
-#         expected = ['Liverpool', 'Manchester United', 'Brighton & Hove Albion']
-#         self.assertEqual(len(output), 3)
-#         self.assertEqual(output, expected)
+class TestTeamScraper(TestCase):
 
-#     def test_names_and_ids_added_to_db(self):
-#         self.fail('write test and function')
+    def setUp(self):
+        self.scraper = TeamScraper()
+
+    def tearDown(self):
+        self.scraper.terminate()
+
+    def test_get_team_ids(self):
+        output = self.scraper.get_team_ids('2019-2020', competition='Premier League')
+        expected = [
+            '822bd0ba', 'b8fd03ef', '19538871', 'cff3d9bb', 'a2d435b3', # liv, mci, mutd, che, lei
+            '361ca564', '8cec06e1', '18bb7c10', '1df6b87e', '943e8050', # tot, wol, ars, shef, bur
+            '33c895d4', 'd3fd31cc', 'b2b47a98', '47c64c55', 'd07537b9', # sou, eve, newc, cry, bri
+            '7c21e445', '8602292d', '4ba7cbea', '2abfe087', '1c781004', # wham, av, bou, wat, nor
+        ]
+        self.assertEqual(len(output), 20)
+        self.assertEqual(output, expected)
+
+    def test_get_team_short_names(self):
+        output = self.scraper.get_team_short_names('2019-2020', competition='Premier League')
+        expected = [
+            'Liverpool', 'Manchester City', 'Manchester United', 'Chelsea', 'Leicester City',
+            'Tottenham', 'Wolves', 'Asrenal', 'Sheffield Utd', 'Burnley',
+            'Southampton', 'Everton', 'Newcastle Utd', 'Crystal Palace', 'Brighton',
+            'West Ham', 'Aston Villa', 'Bournemouth', 'Watford', 'Norwich City'
+        ]
+        self.assertEqual(len(output), 20)
+        self.assertEqual(output, expected)
+
+    def test_get_team_names(self):
+        output = self.scraper.get_team_names(['822bd0ba', '19538871', 'd07537b9']) # liv, mutd, bri
+        expected = ['Liverpool', 'Manchester United', 'Brighton & Hove Albion']
+        self.assertEqual(len(output), 3)
+        self.assertEqual(output, expected)
+
+    def test_try_to_scrape_unsupported_competition_fails(self):
+        with self.assertRaises(ValueError):
+            self.scraper.get_team_ids('2019-2020', competition='fantasy league')
