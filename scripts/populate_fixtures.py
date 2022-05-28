d877d92b43742df369d0a1d30705243d5c19b90c
juancoquet <juancoquet@gmail.com>
2022-05-28 23:51:26 +0100

    populate fixtures for pl and championship 2017-2022

---
+++ scripts/populate_fixtures.py
@@ -0,0 +1,10 @@
+from data_sourcing.db_population.db_population import DBPopulator
+
+
+def run():
+    populator = DBPopulator()
+
+    seasons = ['2017-2018', '2018-2019', '2019-2020', '2020-2021', '2021-2022']
+    competitions = ['Premier League', 'Championship']
+
+    populator.add_fixtures_to_db(seasons, competitions)
