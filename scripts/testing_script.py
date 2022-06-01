e4a7618dcc55eadcbf96b11ae0ee328919b4bdec
juancoquet <juancoquet@gmail.com>
2022-06-01 15:48:16 +0100

    add xGA forecast, alter prediction model, adapt tests

--- scripts/testing_script.py
+++ scripts/testing_script.py
@@ -9,8 +9,12 @@ fixture = Fixture.objects.get(id='7b4b63d0')
 def run():
     print(fixture)
     predictor._calculate_chance_suppression_scores(fixture)
+    print('base xG', predictor._calculate_base_forecast_xGs(fixture))
+    print('base xGA', predictor._calculate_base_forecast_xGAs(fixture))
     print('suppression:', predictor._calculate_chance_suppression_scores(fixture))
     print('conversion:', predictor._calculate_chance_conversion_scores(fixture))
+    print('home performance:', predictor._calculate_home_away_performance(fixture, 'home'))
+    print('away performance:', predictor._calculate_home_away_performance(fixture, 'away'))
     prediction = predictor.generate_prediction(fixture=fixture)
 
     for k, v in prediction.items():
@@ -20,7 +24,9 @@ def run():
 
 
 
-# mci base xG = 2.88
-# av suppression = 0.86
-# mci conversion = 1.17
-# mci performance = 1.25
+# fxG = ((bfxG * conversion) + (o_bfxGA * o_suppression)) / 2 * h_a_performance
+
+# %%
+home_fxG = (((2.88 * 1.17) + (1.42 * 0.86)) / 2) * 1.25 
+away_fxG = (((1.48 * 1.06) + (0.88 * 1.0)) / 2) * 1.0 
+home_fxG, away_fxG
