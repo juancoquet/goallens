3f970ca3964ee295be640b6204417cd927e0bf59
juancoquet <juancoquet@gmail.com>
2022-06-06 21:15:06 +0100

    home page responsive design

--- static/js/home.js
+++ static/js/home.js
@@ -1,5 +1,7 @@
+// resize elements in hero banner for responsive design
+
 const heroHeading = document.querySelector('.hero__heading')
-const heroText = document.querySelector('.hero__text')
+const heroText = document.querySelector('#hero__text')
 
 function setHeroMargins(){
     let margin = heroHeading.offsetHeight / 1.5
@@ -14,3 +16,28 @@ function setHeroMargins(){
 
 document.addEventListener('DOMContentLoaded', setHeroMargins)
 window.addEventListener('resize', setHeroMargins)
+
+
+
+// move buttons for responsive design
+
+const predBtn = document.querySelector('#pred-btn')
+const defaultParentPred = predBtn.parentElement
+const desktopParentPred = document.querySelector('#predictions-text')
+const analysisBtn = document.querySelector('#analysis-btn')
+const defaultParentAnalysis = analysisBtn.parentElement
+const desktopParentAnalysis = document.querySelector('#analysis-text')
+
+function movePredBtn(){
+    if (window.innerWidth >= 900) {
+        desktopParentPred.appendChild(predBtn)
+        desktopParentAnalysis.appendChild(analysisBtn)
+    }
+    else {
+        defaultParentPred.appendChild(predBtn)
+        defaultParentAnalysis.appendChild(analysisBtn)
+    }
+}
+
+document.addEventListener('DOMContentLoaded', movePredBtn)
+window.addEventListener('resize', movePredBtn)
