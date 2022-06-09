ac376b23d40248eb8e106c014e6bc05054165ec0
juancoquet <juancoquet@gmail.com>
2022-06-10 00:56:53 +0100

    add cell probabilites to matrix

--- static/js/prediction_detail.js
+++ static/js/prediction_detail.js
@@ -1,8 +1,29 @@
-// colour matrix cells
+// matrix cells
 
 const cells = document.querySelectorAll('.cell');
 
-function colourCells() {
+
+function insertCellProbability() {
+    cells.forEach((cell) => {
+        let id = cell.id;
+        let homeGoals = id.split('-')[0];
+        let awayGoals = id.split('-')[1];
+        let homeProb = document.querySelector(`#prob_hg_${homeGoals}`).innerHTML.slice(0, -1);
+        let awayProb = document.querySelector(`#prob_ag_${awayGoals}`).innerHTML.slice(0, -1);
+        homeProb = parseFloat(homeProb);
+        awayProb = parseFloat(awayProb);
+        let prob = homeProb * awayProb / 100;
+        let p = document.createElement('p');
+        p.innerHTML = `${prob.toFixed(2)}`;
+        p.classList.add('cell__probability');
+        cell.appendChild(p);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', insertCellProbability);
+
+
+function shadeCells() {
     cells.forEach((cell) => {
         let id = cell.id;
         let homeGoals = id.split('-')[0];
@@ -20,4 +41,4 @@ function colourCells() {
     });
 }
 
-document.addEventListener('DOMContentLoaded', colourCells);
+document.addEventListener('DOMContentLoaded', shadeCells);
