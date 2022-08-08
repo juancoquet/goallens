6a7b61aa1fde4d3738de836e96d4fb28ad874d52
juancoquet <juancoquet@gmail.com>
2022-08-08 15:33:33 +0100

    fix 'today' in daily_script

--- scripts/daily_script.py
+++ scripts/daily_script.py
@@ -10,7 +10,6 @@ def run():
     populator.add_upcoming_predictions_to_db(comps, **PARAMS, within_days=2)
     print('added predictions')
     today = dt.date.today()
-    today = dt.date(2022, 8, 6)
     yesterday = today - dt.timedelta(days=1)
     populator.update_settled_fixtures(yesterday)
     populator.update_settled_fixtures(today)
