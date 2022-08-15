bdd6c610b32562790502a29827485dac20621472
juancoquet <juancoquet@gmail.com>
2022-08-15 11:01:13 +0100

    add date arg on update fixtures script

--- scripts/update_fixtures.py
+++ scripts/update_fixtures.py
@@ -4,13 +4,18 @@ import os
 from params import PARAMS
 from supported_comps import PREDICTION_COMPS
 
-def run():
+def run(on_date:str=None):
     populator = DBPopulator()
     comps = list(PREDICTION_COMPS.keys())
     populator.add_upcoming_predictions_to_db(comps, **PARAMS, within_days=2)
     print('added predictions')
-    today = dt.date.today()
-    yesterday = today - dt.timedelta(days=1)
-    populator.update_settled_fixtures(yesterday)
-    populator.update_settled_fixtures(today)
+    if on_date is not None:
+        on_date = dt.datetime.strptime(on_date, '%Y-%m-%d').date()
+    else:
+        on_date = dt.date.today()
+    day_before = on_date - dt.timedelta(days=1)
+    print(f'updating fixtures for {day_before}')
+    populator.update_settled_fixtures(day_before)
+    print(f'updated fixtures for {on_date}')
+    populator.update_settled_fixtures(on_date)
     print('updated settled fixtures')
